f4ed90224b86135a19adadc86339c492fcb05dab
Fabian Ruff <fabian.ruff@sap.com>
2016-02-29 12:59:26 +0100

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -3,9 +3,17 @@
 set -x -e
 
 vagrant up
-wget -c -O fly http://localhost:8080/api/v1/cli?arch=amd64&platform=darwin
+count=0
+while ! curl -s -f -o /dev/null http://localhost:8080; do
+  [ $count -ge 60 ] && exit 1
+  count=$((count+1))
+  echo "Waiting for http://localhost:8080..."
+  sleep 1
+done
+platform=$(uname | tr '[:upper:]' '[:lower:]')
+wget -c -O fly "http://localhost:8080/api/v1/cli?arch=amd64&platform=$platform"
 chmod +x fly
 ./fly -t vagrant login -c http://localhost:8080 -u concourse -p test
-./fly -t vagrant sp -p test -c pipeline.yml
+./fly -t vagrant sp -n -p test -c pipeline.yml
 ./fly -t vagrant unpause-pipeline -p test
 open http://localhost:8080
